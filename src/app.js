183ea833841d0056ec94e6c6dc4b30d1f07627af
Ridhim06 <56842879+Ridhim06@users.noreply.github.com>
2021-09-23 20:19:47 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,22 +1,32 @@
 const express=require("express");
-const env = require("dotenv");
+// const env = require("dotenv");
+const result=require('dotenv').config();
+// env.config({ path: process.cwd() + 'E:\Quiz\src\.env' });
+if(result.error){
+    throw result.error}
+
+
+// const env=require('dotenv').config({path:'src\.env'});
 const Question=require('./models/questions')
  const Course=require('./models/courses')
- var cors = require('cors')
+ var cors = require('cors');
+const { urlencoded } = require("express");
 
 require("./db/conn");
 const app= express();
 app.use(express.json())
-env.config();
+// env.config({path:'src\.env'});
 app.use(cors());
-const port=8000;
+let port=process.env.PORT;
+console.log("port====>",port)
 
 
 
-// app.get("/",(req,res)=>{
-//     res.send("hello from the other side by ridhim ");
+app.get("/",(req,res)=>{
+    res.send("1. to get courses add '/getCourses' in url.  , 2.To Post or create questions add '/course' in the url .   ,3. To add questions add '/questions' in the url .4. To fetch questions add '/questions' from url ");
+    
 
-// })
+})
 // create a new student
 
 app.post("/questions",(req,res)=>{
@@ -47,6 +57,17 @@ app.get("/getCourses",async(req,res)=>{
     catch(e){
         res.send(e);
 
+    }
+})
+
+app.get("/questions",async(req,res)=>{
+    try{
+        const getQuestions=await Question.find();
+        console.log(getQuestions) ;
+        res.send(getQuestions);
+    }
+    catch(e){
+        res.send(e);
     }
 })
 
